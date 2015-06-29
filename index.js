80ac050ef8d9298a25bb6ab071cbf669305dd9bc
Matt Carpenter <mattcarpenter@gmail.com>
2015-06-28 18:08:21 -0700

    checking in

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-(function() {
-//var pru = require('prussdrv');
 var mpu6050 = require('mpu6050');
+var pru = require('node-pru-extended');
+
 var mpu = new mpu6050();
 mpu.initialize();
 if (mpu.testConnection()) {
@@ -9,13 +9,3 @@ setInterval(function() {
 }, 500);
 }
 //mpu.setSleepEnabled(1);
-return;
-pru.init();
-
-pru.loadDatafile('data.bin');
-pru.execute('text.bin', 908);
-
-pru.waitForInterrupt(function(){
-console.log("Interrupted by PRU");
-});
-})();
