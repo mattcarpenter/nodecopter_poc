4ae9c38e4e1ff60baa66ba3a24d732c74fc675dc
Matt Carpenter <mattcarpenter@gmail.com>
2015-06-29 19:20:51 -0700

    motors module

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var mpu6050 = require('mpu6050');
 var controller = require('./lib/controller');
+var motors = require('./lib/motors');
+var constants = require('./lib/constants');
 
 // init MPU
 var mpu = new mpu6050();
 mpu.initialize();
 controller.initialize();
 
+// init motors
+motors.initialize();
+motors.setArmedStatus(true);
+motors.setMotor(constants.MOTOR_POSITION, 1);
+
 // output loop
 setInterval(function() {
 
 	// read MPU
-	console.log(mpu.getMotion6());
+	//console.log(mpu.getMotion6());
 
 	// read PRU
-	console.log(controller.read());
+	//console.log(controller.read());
 
-}, 500);
+	motors.update();
+
+}, 1);
 
 //mpu.setSleepEnabled(1);
