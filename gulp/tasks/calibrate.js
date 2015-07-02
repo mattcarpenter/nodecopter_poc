827413b4bca871116fde3ac0a8c30e17997fb8b6
root <root@arm.localdomain>
2015-07-02 02:41:22 +0000

    finished calibration gulp task

--- gulp/tasks/calibrate.js
+++ gulp/tasks/calibrate.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var motion = require('../../lib/motion');
+var fs = require('fs');
 
 gulp.task('calibrate', function (cb) {
 	motion.initialize();
-	motion.calibrate().then(function () {
-		console.log('calibration completed');
-		cb();
+	motion.calibrate().then(function (offsets) {
+		console.log('calibration completed with offsets:');
+		console.log(offsets);
+
+		// write offsets to configuration file
+		var fileName = './lib/config/offsets.json';
+		fs.writeFile(fileName, JSON.stringify(offsets, null, 4), cb);	
 	});	
 });
