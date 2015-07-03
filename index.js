ef216d28ca33fe18193d6d2caacaad7d863d931e
Matt Carpenter <mattcarpenter@gmail.com>
2015-07-03 15:26:59 -0700

    checking in

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var controller = require('./lib/controller');
 var motors = require('./lib/motors');
 var motion = require('./lib/motion');
 var constants = require('./lib/constants');
-var ratePID = require('./lib/pid/rate');
+var ratePid = require('./lib/pid/rate');
 
 // initialize r/c controller
 controller.initialize();
@@ -16,8 +16,22 @@ motors.setArmedStatus(true);
 motion.initialize();
 
 // flight control loop
-setInterval(function() {
-	console.log(motion.getAcceleration());
-	motors.update();
-}, 100);
+(function () {
+	var rc;
+
+	setInterval(function() {
+		rc = controller.read();
+
+		console.log(rc);
+
+		// stabilization requires some footroom to work properly
+		if (rc.throttle < config.controller.throttleMin + 100) {
+			console.log('disarm');
+			return motors.setArmedStatus(false);
+		}
+
+
+
+	}, 100);
+})();
 //mpu.setSleepEnabled(1);
